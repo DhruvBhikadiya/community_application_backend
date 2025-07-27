42358474ef04d9e42ae3f10fe12602bc1f7a3cf9
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2025-07-27 23:46:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.use('/api', require('./routes/index.js'));
 
+app.get('/', async (req,res) => {
+  return res.status(400).json({"server is running"});
+}
+
 app.listen(process.env.PORT, (e) => {
   e ? console.log(e) : console.log(`Server is running on port :- ${process.env.PORT}`);
 });
